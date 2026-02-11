899e2a6221ae80c06847c7b0ba12b793233fa46a
etboodXJ <etbood@gmail.com>
2026-02-11 19:10:18 +0800

    Add SuiClientProvider to fix SuiClientContext error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { WalletProvider } from '@mysten/dapp-kit'
+import { WalletProvider, SuiClientProvider } from '@mysten/dapp-kit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NETWORK_CONFIG } from './utils/config'
 import App from './App.tsx'
 import './i18n'
 
 const queryClient = new QueryClient()
 
+function Providers({ children }: { children: React.ReactNode }) {
+  const networks = {
+    testnet: { url: NETWORK_CONFIG.testnet.fullnode },
+    mainnet: { url: NETWORK_CONFIG.mainnet.fullnode },
+  }
+
+  return (
+    <SuiClientProvider networks={networks} defaultNetwork="testnet">
+      <WalletProvider autoConnect={true}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </WalletProvider>
+    </SuiClientProvider>
+  )
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WalletProvider autoConnect={true}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </WalletProvider>
+    <Providers>
+      <App />
+    </Providers>
   </StrictMode>,
 )
