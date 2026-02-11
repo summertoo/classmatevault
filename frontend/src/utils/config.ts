910f8921d6f1e9c83c8fe9238b93ec450fa2775d
etboodXJ <etbood@gmail.com>
2026-02-11 19:41:11 +0800

    Add testnet and mainnet package IDs to config

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -10,6 +10,6 @@ export const NETWORK_CONFIG = {
 };
 
 export const CONTRACT_PACKAGE_ID = {
-  testnet: '', // 部署后填入
-  mainnet: '', // 部署后填入
+  testnet: '0x15a5f901aa64a339e9dd55b33a42f55b70072367edb18aaad186937eb6d96be6', // 部署后填入
+  mainnet: '0x9bdc8526e164e7a4eda87f646660a1ae32735c8a4a0ed54b2130368909c536a8', // 部署后填入
 };
