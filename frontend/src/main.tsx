3ce491111ee7252681314b62c60582441faa98c2
etboodXJ <etbood@gmail.com>
2026-02-11 19:08:17 +0800

    Fix WalletProvider missing error in frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { WalletProvider } from '@mysten/dapp-kit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
+import './i18n'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WalletProvider autoConnect={true}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WalletProvider>
   </StrictMode>,
 )
